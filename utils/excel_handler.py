6565a2f89f0a216418b4227b4b7d85b87152c97d
kimchinam09 <146310311+kimchinam09@users.noreply.github.com>
2025-06-07 16:31:16 +0700

    change timestamp, clean excelhandler

--- utils/excel_handler.py
+++ utils/excel_handler.py
@@ -2,9 +2,7 @@
 import os
 from openpyxl import Workbook, load_workbook
 from datetime import datetime
-from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
-import os
 
 def append_to_excel(file_path, sender_name, department, machine, timestamp, image_path=None):
     if os.path.exists(file_path):
@@ -20,21 +18,3 @@ def append_to_excel(file_path, sender_name, department, machine, timestamp, imag
     wb.save(file_path)
 
 
-def write_to_excel(file_path, user_id, department, machine, image_path):
-    if not os.path.exists(file_path):
-        wb = Workbook()
-        ws = wb.active
-        ws.append(["User ID", "Department", "Machine", "Date", "Image Path"])
-    else:
-        wb = load_workbook(file_path)
-        ws = wb.active
-
-    ws.append([
-        user_id,
-        department,
-        machine,
-        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        image_path
-    ])
-
-    wb.save(file_path)
