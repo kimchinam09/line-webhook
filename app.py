28f9a8528be41bfb8bbbc2e451c8e613542fb6f7
kimchinam09 <146310311+kimchinam09@users.noreply.github.com>
2025-06-07 20:05:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ def handle_text_message(event):
         parts = text.split("-", 1)
         department = parts[0].strip()
         machine = parts[1].strip()
+        machine = machine.lower().replace(" ", "_")
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
 
         # Save basic info
@@ -69,7 +70,7 @@ def handle_image_message(event):
     append_to_excel(EXCEL_FILE_PATH, sender_name, None, None, timestamp, image_path=TEMP_IMG_PATH)
 
  # Upload ảnh và Excel lên OneDrive
-    image_filename = f"CIL bot data/uploaded_images/{user_id}_{timestamp}.jpg"
+    image_filename = f"CIL bot data/uploaded_images/{machine}_{timestamp}.jpg"
     onedrive_client.upload_file(TEMP_IMG_PATH, image_filename)
     onedrive_client.upload_file(EXCEL_FILE_PATH, "CIL bot data/data.xlsx")
 
